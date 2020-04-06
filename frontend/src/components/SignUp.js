1508a143bf905d860e8bf9a32aa9a4a884dff4a4
emi.luciani18@gmail.com <59806361+emiluce@users.noreply.github.com>
2020-04-06 10:35:55 +0200

    commit
    Signed-off-by: emi.luciani18@gmail.com <59806361+emiluce@users.noreply.github.com>

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -5,8 +5,15 @@ class SignUp extends React.Component {
   render() {
     return (
       <div>
-        <h1>test@test.com</h1>
-        <input type="email" name="email" onchange="myFunction(this.value)" />
+        <h1>{this.value}</h1>
+        <form>
+          <input type="email" name="email" onchange="myFunction(this.value)" placeholder="E-mail" />
+          <input type="password" name="password" onchange="myFunction(this.value)" placeholder="password" />
+          <input type="passwordconf" name="passwordconf" onchange="myFunction(this.value)" placeholder="Confirm password" />
+          <input type="name" name="name" onchange="myFunction(this.value)" placeholder="name" />
+          <input type="lastname" name="lastname" onchange="myFunction(this.value)" placeholder="lastname" />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
